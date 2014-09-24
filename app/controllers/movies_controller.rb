0f69d83346736c1b98896823b018cee4cd2ebe8c
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2014-09-24 20:53:46 +0530

    Fix null value being saved to movies user_id, and allow only the owner of the post(movie) to edit/update/delete a movie.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,11 @@
 class MoviesController < ApplicationController
 	before_filter :authenticate_user!
+  before_filter :find_movie, only: [:edit, :update, :destroy]
+  def find_movie
+    @movie = current_user.movies.find_by_id(params[:id])
+    
+  end
+
   def index
   	@movies = Movie.all
   end
@@ -8,8 +14,10 @@ class MoviesController < ApplicationController
   end
 
   def create
-  	#render :text => params[:movie].inspect
-    @movie = Movie.create(params[:movie])  	
+  	#render :text => params[:movie].inspect    
+    @movie = Movie.create(params[:movie]) 
+    #assigns the current userId to the movie's userid.
+    @movie.user_id = current_user.id 	
     #@movie.new(params[:movie])
     #@movie.save
     if(@movie.save)
@@ -19,6 +27,27 @@ class MoviesController < ApplicationController
     end
   end
   def edit
-  	@movie = Movie.find(params[:id])
+  	if @movie
+      redirect_to(movies_path,:notice => "Movie updated successfully") 
+    else
+      redirect_to(movies_path, :alert => "You can only update the post created by you !")
+    end     
+  end
+  def update   
+    if @movie
+      redirect_to(movies_path,:notice => "Movie updated successfully")     
+    else
+       redirect_to(movies_path, :alert => "You can only update the post created by you !")
+    end
+    
+  end
+  def destroy   
+    if  @movie
+      @movie.destroy
+      redirect_to(movies_path,:notice => "Movie deleted successfully")     
+    else
+      redirect_to(movies_path, :alert => "You can only delete the post created by you !")
+    end
+    
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
   belongs_to :user
   belongs_to :genre
   belongs_to :language
-  attr_accessible :description, :title, :genre_id, :language_id, :id
+  attr_accessible :description, :title, :genre_id, :language_id, :user_id
   validates :title, presence: true   			
   validates :description, presence: true
 end
